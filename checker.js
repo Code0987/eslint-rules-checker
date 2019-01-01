000a29d3461093de18988d1fddf70506d9e3ca7d
code0987 <durgapalneeraj@gmail.com>
2019-01-01 21:25:32 +0530

    Initial commit.

---
+++ checker.js
@@ -0,0 +1,116 @@
+const fs = require('fs');
+const path = require('path');
+const eslint = require('eslint');
+const isAbsolute = require('path-is-absolute');
+
+function _isDir(path) {
+  try {
+    var stat = fs.lstatSync(path);
+    return stat.isDirectory();
+  } catch (e) {
+    // lstatSync throws an error if path doesn't exist
+    return false;
+  }
+}
+
+function resolveConfigFile(input) {
+  if (input) {
+    if (!_isDir(input)) {
+      if (isAbsolute(input)) {
+        return input;
+      }
+      return path.join(process.cwd(), input);
+    }
+    return require(path.join(path.join(process.cwd(), input), 'package.json')).main;
+  }
+  return require(path.join(process.cwd(), 'package.json')).main;
+}
+
+function getConfig(input) {
+  if (input) {
+    const cliEngine = new eslint.CLIEngine({
+      useEslintrc: false,
+      configFile: resolveConfigFile(input)
+    });
+    return cliEngine.getConfigForFile();
+  } else {
+    const cliEngine = new eslint.CLIEngine({
+      useEslintrc: true
+    });
+    return cliEngine.getConfigForFile();
+  }
+}
+
+function check(ops) {
+  ops = Object.assign({}, {
+    input: null,
+    output: null,
+    current: true,
+    unused: true,
+    deprecated: true,
+    print: true
+  }, ops);
+
+  const config = getConfig(ops.input);
+
+  const allRules = new Map([...eslint.linter.getRules()]);
+
+  let currentRuleNames = Object.keys(config.rules);
+  let allRuleNames = [...allRules.keys()];
+
+  const deprecatedRuleNames =
+    currentRuleNames.filter(x =>
+      allRules.get(x)
+      && allRules.get(x).meta
+      && allRules.get(x).meta.deprecated);
+
+  const unusedRuleNames =
+    allRuleNames.filter(x =>
+      !(x in currentRuleNames));
+
+  let report =
+    "eslint Rules Checker Report"
+    + "\n---"
+    ;
+
+  if (ops.current) {
+    report += "\n\nCurrent"
+      + "\nTotal: " + currentRuleNames.length
+      + "\n" + currentRuleNames.join("  \n")
+      ;
+  }
+
+  if (ops.deprecated) {
+    report += "\n\nUnused"
+      + "\nTotal: " + unusedRuleNames.length
+      + "\n" + unusedRuleNames.join("  \n")
+      ;
+  }
+
+  if (ops.deprecated) {
+    report += "\n\nDeprecated"
+      + "\nTotal: " + deprecatedRuleNames.length
+      + "\n" + deprecatedRuleNames.join("  \n")
+      ;
+  }
+
+  if (ops.output) {
+    require('fs').writeFile(
+      ops.output,
+      report,
+      function (err) {
+        if (err) {
+          console.error(':(');
+        }
+      }
+    );
+  }
+
+  if (ops.print) {
+    console.log(report);
+  }
+}
+
+module.exports = {
+  check
+};
---
+++ index.js
@@ -0,0 +1,36 @@
+#!/usr/bin/env node
+
+const argv = require('yargs')
+
+  .alias('i', 'input')
+  .nargs('i', 1)
+  .describe('i', 'eslintrc file or eslintrc project dir')
+
+  .alias('o', 'output')
+  .nargs('o', 1)
+  .describe('o', 'output file')
+
+  .boolean('c')
+  .alias('c', 'current')
+  .describe('c', 'current rules')
+
+  .boolean('u')
+  .alias('u', 'unused')
+  .describe('u', 'unused rules')
+
+  .boolean('d')
+  .alias('d', 'deprecated')
+  .describe('d', 'deprecated rules')
+
+  .boolean('p')
+  .alias('p', 'print')
+  .describe('p', 'print report')
+  
+  .help('h')
+  .alias('h', 'help')
+
+  .argv;
+
+const checker = require("./checker");
+
+checker.check(argv);
